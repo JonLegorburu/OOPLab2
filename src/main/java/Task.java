d39e73d91542f84e66b79d7a4ff208fbd2efefab
JonLegorburu <jon.legorburu@alumni.mondragon.edu>
2021-11-08 12:16:33 +0100

    Exercise 2

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,8 +2,8 @@ import java.time.LocalDateTime;
 
 public class Task {
     private String taskName;
-    private String TaskDescription;
-    private enum taskCategory{
+    private String taskDescription;
+    enum TaskCategory{
         cat1,
         cat2,
         cat3
@@ -11,5 +11,24 @@ public class Task {
 
     private LocalDateTime taskDateTimeStart;
     private LocalDateTime taskDateTimeStop; //The deadline
+
+
+    //Empty constructor
+    public Task() {
+    }
+
+    //Arguments constructor
+
+    TaskCategory taskCategory;
+
+    public Task(String taskName, String taskDescription, TaskCategory taskCategory, LocalDateTime taskDateTimeStop) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.taskCategory=taskCategory;
+        this.taskDateTimeStart= LocalDateTime.now();
+        this.taskDateTimeStop = taskDateTimeStop;
+    }
+
+    
 
 }
